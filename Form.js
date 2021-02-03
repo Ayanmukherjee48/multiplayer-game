32f4b2045ac32e759312eda3b6755e9651ee14ce
Ayanmukherjee48 <76084814+Ayanmukherjee48@users.noreply.github.com>
2021-02-03 09:59:41 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,32 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title=createElement("h2");
+        title.html("Car Racing Game");
+        title.position(150,0);
+        
+        var input=createInput("name")
+        input.position(150,150);
+
+        var button=createButton("PLAY")
+        button.position(200,200)
+
+        var greeting=createElement("h3")
+
+        button.mousePressed(function(){
+            input.hide()
+            button.hide()
+            
+            var name=input.value()
+            playerCount+=1
+
+            player.update(name)
+            player.updateCount(playerCount)
+
+            greeting.html("WELCOME " +name)
+            greeting.position(150,150);
+        })
+    }
+}
